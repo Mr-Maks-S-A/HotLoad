50aa102a0d27eaa012c8ecfda2ee5b9d0c848ea4
Mr-Maks-S-A <mrfarkses@gmail.com>
2025-05-22 20:31:51 +0300

    Base 2

--- Internal LIb/BaseHotLoadLIb/code/BaseHLoadLib.cpp
+++ Internal LIb/BaseHotLoadLIb/code/BaseHLoadLib.cpp
@@ -1,5 +1,5 @@
 // #include "lib/BaseHLoadLib.hpp"
-#include <BaseHLoadLib.hpp>
+#include <lib/BaseHLoadLib.hpp>
 
 int Foo::GetTheAnswer() const {
   return m_answer;
--- Src/main.cpp
+++ Src/main.cpp
@@ -1,4 +1,4 @@
-#include <BaseHLoadLib.hpp>
+#include <lib/BaseHLoadLib.hpp>
 #include <iostream>
 
 /// @brief The main() function in C/C++ is the main function from which the program execution begins. It is the entry point of the program where the operating system transfers control and from where the code execution begins.
